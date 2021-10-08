848f0fef409de7d8d16e1bbee544c0fee2f274df
Anam Asif <anam.asif285@gmail.com>
2021-10-08 14:18:45 +0100

    Acrostic Solution

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv;
-require("wordhub")
 
 const main = ()=>{
-    let words =[]
-    words = (argv.text).split(' ')
-    console.log(words)
-    let initial =[]
-    words.map((item)=>{
-        initial.push(item.charAt(0))
+    let words = (argv.text).toLowerCase().split(' ')
+    let msg =(argv.msg).toLowerCase();
+    let initial = words.map((item => item.charAt(0)));
+    let last = words.map((item => item.charAt(item.length - 1)));
+    let condition = true
+    
+
+    msg = msg.trim().split(" ").join("").split("");
+    msg.map((item) => {if (initial.includes(item)){
+        initial.splice(initial.indexOf(item), 1);
+        return condition= true 
+        }
+        else if (last.includes(item)){
+            last.splice(last.indexOf(item), 1)
+        }
+        else condition= false;
     })
-    let scrambleWord =initial.join(' ')
-    console.log(scrambleWord)
-    console.log(findWords(scrambleWord))
+    console.log(condition ? "Msg Exist": "Msg Does not Exist")
+    
 }
+
+
 
 main()
 
